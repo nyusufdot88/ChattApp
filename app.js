0e8e46f4a35cb68a2a3a27c858255614d1a3a1b8
[Johan Håkansson] <[johan1hakansson@gmail.com]>
2024-04-04 14:13:36 +0200

    Fixat mongoose och lagt till så att vi kan connecta till databasen. // Johan

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from 'express';
 import router from './src/router/router.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
+
+//För att connect till databasen.
+const mongoDB =
+	'mongodb+srv://johan1hakansson:V2SzLxUaFGj61TVk@cluster0.ftnhogo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongoDB);
+
+// Logga till konsolen om vi är anslutna till databasen
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+	console.log('Connected to MongoDB');
+});
 
 //mount router
 app.use(router);
