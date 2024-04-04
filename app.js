41bbcadd78977914b9ae8f581a83614dc83c2192
Konvvit <stackov3rflow@hotmail.com>
2024-04-04 14:36:52 +0200

    Test
    Test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import router from './src/router/router.js';
-import mongoose from 'mongoose';
+import express from "express";
+import router from "./src/router/router.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,15 +8,15 @@ app.use(express.json());
 
 //För att connect till databasen.
 const mongoDB =
-	'mongodb+srv://johan1hakansson:V2SzLxUaFGj61TVk@cluster0.ftnhogo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  "mongodb+srv://johan1hakansson:V2SzLxUaFGj61TVk@cluster0.ftnhogo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(mongoDB);
 
 // Logga till konsolen om vi är anslutna till databasen
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function () {
-	console.log('Connected to MongoDB');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
 });
 
 //mount router
@@ -24,5 +24,7 @@ app.use(router);
 
 /**Initialization */
 app.listen(5000, async () => {
-	console.log('Server is live');
+  console.log("Server is live");
 });
+
+//Test//
