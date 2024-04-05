5c365d12784128cf4ef6863dd81acbf13c3580e5
[Johan Håkansson] <[johan1hakansson@gmail.com]>
2024-04-05 21:46:36 +0200

    Fixat tills så att man kan skriva in användarnamn också och gjort så att strukturen är lite bättre i projektet

--- src/model/messageModel.js
+++ src/model/messageModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const messageSchema = new mongoose.Schema({
-	message: String,
+	message: { type: String, required: true, minLength: 1, maxLength: 100 },
 	sender: String,
 	timestamp: { type: Date, default: Date.now },
 	channalID: { type: String },
